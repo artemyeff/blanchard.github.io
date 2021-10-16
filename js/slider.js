ed57e862e57877530f3a816b34b5801bf3f22686
Денис Артемьев <artemievdenis48@gmail.com>
2021-10-16 19:51:01 +0300

    v.1.9

--- js/slider.js
+++ js/slider.js
@@ -64,64 +64,125 @@
               type: 'fraction',
             },
           });
+
+
+
+
+
+    
         
         
 
 
         // swiper3
-        let mySwiper2 = function () {
-	        if (window.innerWidth >= 750) {
-		var swiper2 = new Swiper('.container-slider3', {
+//         let mySwiper2 = function () {
+// 	        if (window.innerWidth >= 750) {
+// 		var swiper2 = new Swiper('.container-slider3', {
 
-			breakpoints: {
-				750: {
-					slidesPerView: 2,
-					spaceBetween: 34,
-				},
-				970: {
-					slidesPerView: 2,
-					spaceBetween: 50,
-				},
-				1430: {
-					slidesPerView: 3,
-					slidesPerColumn: 1,
-					slidesPerGroup: 1,
-					spaceBetween: 50,
-				},
-			},
-			autoplay: {
-				delay: 6000,
-				disableOnInteraction: false,
-			},
+// 			breakpoints: {
+// 				750: {
+// 					slidesPerView: 2,
+// 					spaceBetween: 34,
+// 				},
+// 				970: {
+// 					slidesPerView: 2,
+// 					spaceBetween: 50,
+// 				},
+// 				1300: {
+// 					slidesPerView: 3,
+// 					slidesPerColumn: 1,
+// 					slidesPerGroup: 1,
+// 					spaceBetween: 50,
+// 				},
+// 			},
+// 			autoplay: {
+// 				delay: 6000,
+// 				disableOnInteraction: false,
+// 			},
 
-			navigation: {
-				nextEl: '.swiper-button-next-publications',
-				prevEl: '.swiper-button-prev-publications',
-			},
+// 			navigation: {
+// 				nextEl: '.swiper-button-next-publications',
+// 				prevEl: '.swiper-button-prev-publications',
+// 			},
 
 
-			pagination: {
-				el: '.swiper-pagination-publications',
-				clickable: true,
-				type: 'fraction',
-			},
+// 			pagination: {
+// 				el: '.swiper-pagination-publications',
+// 				clickable: true,
+// 				type: 'fraction',
+// 			},
 
-		});
+// 		});
 
-		swiper2.on('resize', function () {
-			if (innerWidth < 750) {
-				swiper2.destroy();
-			}
-		});
-	}
-}
+// 		swiper2.on('resize', function () {
+// 			if (innerWidth < 750) {
+// 				swiper2.destroy();
+// 			}
+// 		});
+// 	}
+// }
 
+//         window.addEventListener('resize', () => {
+//             mySwiper2();
+//         })
+
+//         mySwiper2();
+
+
+
+
+
+
+
+        const sliderDesktop2 = document.querySelector('.container-slider3');
+        let swiper2;
+        const desctopSlider = () => {
+          if (window.innerWidth >= 670 && sliderDesktop2.dataset.desktop == 'true') {
+            swiper2 = new Swiper(sliderDesktop3, {
+              slideClass: ('section-publications__item'),
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 10,
+              pagination: {
+                el: '.swiper-pagination-publications',
+                type: 'fraction',
+              },
+              navigation: {
+                nextEl: '.swiper-button-next-publications',
+                prevEl: '.swiper-button-prev-publications',
+              },
+              breakpoints:{
+                750: {
+                  slidesPerView: 2,
+                  spaceBetween: 34,
+                },
+                970:{
+                  slidesPerView: 2,
+            			spaceBetween: 50,
+                },
+      
+                1300:{
+                  slidesPerView: 3,
+                  slidesPerColumn: 1,
+                  slidesPerGroup: 1,
+                  spaceBetween: 50,
+                }
+              }
+            })
+            sliderDesktop3.dataset.desktop == 'false'
+          }
+          if (window.innerWidth <= 670) {
+            sliderDesktop2.dataset.mobile = 'false';
+              if (sliderDesktop2.classList.contains('swiper-container-initialized')) {
+                swiper3.destroy();
+              }
+          }
+        }
+        desctopSlider();
         window.addEventListener('resize', () => {
-            mySwiper2();
+          desctopSlider();
         })
-
-        mySwiper2();
-
+      
 
 
         // swiper4
@@ -164,7 +225,7 @@
 
 
 
-        // events slider
+              // events slider
 
 const mobileSlider = document.querySelector('.section-events__content');
 
@@ -209,6 +270,7 @@ window.addEventListener('resize', () => {
 
 
 
+        
 
 
 
@@ -224,28 +286,5 @@ window.addEventListener('resize', () => {
 
 
 
-// var swiper;
-
-// function resizeScrenn() {
-//     if ($(window).width() >= 1200) {
-//         if ($(".container-slider3").length > 0) {
-//             swiper = new Swiper('#swiper-container', {
-//                 direction: 'vertical', // вертикальный слайдер
-//                 slidesPerView: 1,
-//                 spaceBetween: 0,
-//                 loop: false,
-//                 touchRatio: 0,
-//                 slidesPerGroup: 1,
-//             });
-//         }
-//     } else {
-//         swiper.destroy(); // или swiper.autoplay.stop();
-//     }
-// }
 
 
-// resizeScrenn();
-
-// $(window).resize(function () {
-//     resizeScrenn();
-// });
